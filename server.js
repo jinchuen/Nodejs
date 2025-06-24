2c5d9b288622a3f1a1d5c6d43969f3bd7e3e6cf7
KHOR JIN CHUEN <khorjinchuen2006@gmail.com>
2025-06-24 09:12:35 +0800

    Created the base api for it

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
 
-const posts = [
-    {
-        username: 'Abing',
-        title: 'Sup'
-    },
-    {
-        username: 'Ahuang',
-        title: 'huang'
-    },    
-]
+app.use(express.json());
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+const users = [];
+
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+
+app.post('/users', async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const user = { name: req.body.name, password: hashedPassword };
+  users.push(user);
+  res.status(201).send();
+});
+
+app.post('/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if(user == null)
+        return res.status(400).send('User not found');
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Login successful');
+        } else {
+            res.status(400).send('Invalid password');
+        }
+    } catch {
+        res.status(500).send();
+    }
 })
 
-app.listen(3000)
+app.listen(3000);
